feb38810ee2a587458412ee8a2634a6bc3b3fd14
D-Rock <derek.gallo@gmail.com>
2012-12-04 12:51:00 -0500

    Setup simple silex based endpoint for a user

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Testing Mongo Connection...";
+ini_set('display_errors', 'On');
 
 # get the mongo db name out of the env
 if(!$mongo_url = getenv("MONGOHQ_URL"))
@@ -11,4 +11,29 @@ $mongo_url_parts = parse_url($mongo_url);
 $dbname = str_replace("/", "", $mongo_url_parts["path"]);
 
 # connect
-$m   = new Mongo($mongo_url);
+$mongo   = new Mongo($mongo_url);
+$db = $mongo->$dbname;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['mongo'] = $mongo;
+
+$app->get('/api/user', function (\Symfony\Component\HttpFoundation\Request $request) use ($app, $db) {
+
+    if(!$user = $db->users->findOne(array('username' => $request->getUser(), 'password' => $request->getPassword())))
+    {
+        $app->abort(401, 'Authentication Failed');
+    }
+
+    //add the id fields MemberFuse is looking for
+    $user['external_id'] = (string)$user['_id'];
+
+    return $app->json($user);
+});
+
+$app->error(function(\Exception $e, $code) use ($app) {
+    return $app->json(array('error' => $e->getMessage()), $code);
+});
+
+$app->run();
