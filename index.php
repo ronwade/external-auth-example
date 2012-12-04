3b8b2f576b124b3094873874b296a7f4141ee7d6
D-Rock <derek.gallo@gmail.com>
2012-12-04 10:57:45 -0500

    Adding php mongo driver for heroku

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "Hello World";
+echo "Testing Mongo Connection...";
+
+# get the mongo db name out of the env
+$mongo_url = parse_url(getenv("MONGO_URL"));
+$dbname = str_replace("/", "", $mongo_url["path"]);
+
+# connect
+$m   = new Mongo(getenv("MONGO_URL"));
