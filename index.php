206e9e53306343ba35f47fdd6daace6da794c341
D-Rock <derek.gallo@gmail.com>
2012-12-04 11:09:13 -0500

    Update mongo env var for heroku

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 echo "Testing Mongo Connection...";
 
 # get the mongo db name out of the env
-$mongo_url = parse_url(getenv("MONGO_URL"));
+$mongo_url = parse_url(getenv("MONGOHQ_URL"));
 $dbname = str_replace("/", "", $mongo_url["path"]);
 
 # connect
-$m   = new Mongo(getenv("MONGO_URL"));
+$m   = new Mongo(getenv("MONGOHQ_URL"));
