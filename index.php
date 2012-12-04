26ac4a0e1b0af92615ef4f250cbddb6345eb5e16
D-Rock <derek.gallo@gmail.com>
2012-12-04 16:51:22 -0500

    heroku debugging

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $app->get('/api/user', function (\Symfony\Component\HttpFoundation\Request $requ
     $user = null;
 
 
-    print_r(getallheaders());die();
+    print_r($_SERVER);die();
 
     //if an access token was presented, attempt to authenticate with that
     //this could have been from the SSO workflow with MemberFuse
